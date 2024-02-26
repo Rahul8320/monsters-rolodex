9415980919b37f8c08d94557e98a01ded505b099
Rahul Dey <rahuldey832000@gmail.com>
2024-02-26 23:26:05 +0530

    Update Counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(26);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h2>Current Count: {counter}</h2>
+
+        <button onClick={() => setCounter((prev) => prev + 5)}>
+          Update Counter
+        </button>
       </header>
     </div>
   );
